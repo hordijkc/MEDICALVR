d6f2d6fb98ff1e3d8d13bb80869b6f7377f6bc58
Chris Hordijk <hordijk.chris@gmail.com>
2019-06-21 14:00:39 +0200

    basics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :subscribe]
 
   def home
+  end
+
+  def subscribe
   end
 end
